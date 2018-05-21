0a0a523b7b120f6b6fa6b9fc5a2d53263d3a2426
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2018-05-21 13:12:12 +0200

    PM: wakeup: Use pr_debug() for the "aborting suspend" message
    The message printed by pm_wakeup_pending() on wakeup detection is
    not very useful if someone is not interested specifically in
    debugging wakeup, so turn it into a pm_debug() one.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -964,7 +964,7 @@ bool pm_wakeup_pending(void)
 	spin_unlock_irqrestore(&events_lock, flags);
 
 	if (ret) {
-		pr_info("PM: Wakeup pending, aborting suspend\n");
+		pr_debug("PM: Wakeup pending, aborting suspend\n");
 		pm_get_active_wakeup_sources(suspend_abort,
 					     MAX_SUSPEND_ABORT_LEN);
 		log_suspend_abort_reason(suspend_abort);
