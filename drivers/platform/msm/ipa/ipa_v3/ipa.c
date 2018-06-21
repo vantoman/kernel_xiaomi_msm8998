85b13abb99398b83a66f54151a13b501ccb7fc4a
Demon Singur <demonsingur@gmail.com>
2018-06-21 06:44:04 +0000

    ipa_v3: block ipa_ws wakelocks
    Change-Id: I984776eb1ef9b8629be0bde7b89f0f3a134d1c53

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -3784,6 +3784,7 @@ void ipa3_dec_client_disable_clks(struct ipa_active_client_logging_info *id)
  */
 void ipa3_inc_acquire_wakelock(void)
 {
+#if 0
 	unsigned long flags;
 
 	spin_lock_irqsave(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
@@ -3791,6 +3792,7 @@ void ipa3_inc_acquire_wakelock(void)
 	IPADBG_LOW("active wakelock ref cnt = %d\n",
 		ipa3_ctx->wakelock_ref_cnt.cnt);
 	spin_unlock_irqrestore(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
+#endif
 }
 
 /**
@@ -3803,6 +3805,7 @@ void ipa3_inc_acquire_wakelock(void)
  */
 void ipa3_dec_release_wakelock(void)
 {
+#if 0
 	unsigned long flags;
 
 	spin_lock_irqsave(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
@@ -3810,6 +3813,7 @@ void ipa3_dec_release_wakelock(void)
 	IPADBG_LOW("active wakelock ref cnt = %d\n",
 		ipa3_ctx->wakelock_ref_cnt.cnt);
 	spin_unlock_irqrestore(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
+#endif
 }
 
 int ipa3_set_required_perf_profile(enum ipa_voltage_level floor_voltage,
@@ -4986,9 +4990,11 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 		goto fail_nat_dev_add;
 	}
 
+#if 0
 	/* Create a wakeup source. */
 	wakeup_source_init(&ipa3_ctx->w_lock, "IPA_WS");
 	spin_lock_init(&ipa3_ctx->wakelock_ref_cnt.spinlock);
+#endif
 
 	/* Initialize IPA RM (resource manager) */
 	result = ipa_rm_initialize();
