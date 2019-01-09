3ab69febb681b4ff2cc80ebb3eebf431d5bc9ba7
kdrag0n <dragon@khronodragon.com>
2019-01-08 19:29:17 -0800

    sched/tune: use dummy functions when Dynamic Schedtune Boost is disabled
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3603,6 +3603,22 @@ int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
 int do_prefer_idle(char *st_name, u64 prefer_idle);
 
+#else /* !CONFIG_DYNAMIC_STUNE_BOOST */
+static inline int do_stune_boost(char *st_name, int boost, int *slot)
+{
+	return 0;
+}
+
+static inline int do_stune_sched_boost(char *st_name, int *slot)
+{
+	return 0;
+}
+
+static inline int reset_stune_boost(char *st_name, int slot)
+{
+	return 0;
+}
+
 static inline int do_prefer_idle(char *st_name, u64 prefer_idle)
 {
 	return 0;
