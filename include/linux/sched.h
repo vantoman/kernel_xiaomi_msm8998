388af79bfb00518d6508ae135f483d06cc526c90
Tyler Nijmeh <tylernij@gmail.com>
2019-06-11 05:42:56 -0400

    sched/tune: Add support for dynamic prefer_idle

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3601,6 +3601,12 @@ void cpufreq_remove_update_util_hook(int cpu);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
+int do_prefer_idle(char *st_name, u64 prefer_idle);
+
+static inline int do_prefer_idle(char *st_name, u64 prefer_idle)
+{
+	return 0;
+}
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 #endif
