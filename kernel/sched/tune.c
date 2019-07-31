0d3c3c7fc0471b29062d5a91977a30b6b45feeb8
celtare21 <celtare21@gmail.com>
2019-07-31 17:20:45 +0000

    sched: tune: Convert locking to rq_flags
    *As per https://github.com/celtare21/kernel_essential_msm8998-1/commit/49b6faece1b9f6054e9fba2dbb4334c20180aabd
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -384,7 +384,7 @@ schedtune_tasks_update(struct task_struct *p, int cpu, int idx, int task_count)
 void schedtune_enqueue_task(struct task_struct *p, int cpu)
 {
 	struct boost_groups *bg = &per_cpu(cpu_boost_groups, cpu);
-	unsigned long irq_flags;
+	struct rq_flags rf;
 	struct schedtune *st;
 	int idx;
 
@@ -405,7 +405,7 @@ void schedtune_enqueue_task(struct task_struct *p, int cpu)
 	 * interrupt to be disabled to avoid race conditions for example on
 	 * do_exit()::cgroup_exit() and task migration.
 	 */
-	raw_spin_lock_irqsave(&bg->lock, irq_flags);
+	raw_spin_lock_irqsave(&bg->lock, rf.flags);
 	rcu_read_lock();
 
 	st = task_schedtune(p);
@@ -414,7 +414,7 @@ void schedtune_enqueue_task(struct task_struct *p, int cpu)
 	schedtune_tasks_update(p, cpu, idx, ENQUEUE_TASK);
 
 	rcu_read_unlock();
-	raw_spin_unlock_irqrestore(&bg->lock, irq_flags);
+	raw_spin_unlock_irqrestore(&bg->lock, rf.flags);
 }
 
 int schedtune_can_attach(struct cgroup_taskset *tset)
@@ -422,7 +422,7 @@ int schedtune_can_attach(struct cgroup_taskset *tset)
 	struct task_struct *task;
 	struct cgroup_subsys_state *css;
 	struct boost_groups *bg;
-	unsigned long irq_flags;
+	struct rq_flags rf;
 	unsigned int cpu;
 	struct rq *rq;
 	int src_bg; /* Source boost group index */
@@ -440,10 +440,10 @@ int schedtune_can_attach(struct cgroup_taskset *tset)
 		 * conditions with migration code while the task is being
 		 * accounted
 		 */
-		rq = lock_rq_of(task, &irq_flags);
+		rq = lock_rq_of(task, &rf);
 
 		if (!task->on_rq) {
-			unlock_rq_of(rq, task, &irq_flags);
+			unlock_rq_of(rq, task, &rf);
 			continue;
 		}
 
@@ -464,7 +464,7 @@ int schedtune_can_attach(struct cgroup_taskset *tset)
 		 */
 		if (unlikely(dst_bg == src_bg)) {
 			raw_spin_unlock(&bg->lock);
-			unlock_rq_of(rq, task, &irq_flags);
+			unlock_rq_of(rq, task, &rf);
 			continue;
 		}
 
@@ -479,7 +479,7 @@ int schedtune_can_attach(struct cgroup_taskset *tset)
 		bg->group[dst_bg].tasks += 1;
 
 		raw_spin_unlock(&bg->lock);
-		unlock_rq_of(rq, task, &irq_flags);
+		unlock_rq_of(rq, task, &rf);
 
 		/* Update CPU boost group */
 		if (bg->group[src_bg].tasks == 0 || bg->group[dst_bg].tasks == 1)
@@ -505,7 +505,7 @@ void schedtune_cancel_attach(struct cgroup_taskset *tset)
 void schedtune_dequeue_task(struct task_struct *p, int cpu)
 {
 	struct boost_groups *bg = &per_cpu(cpu_boost_groups, cpu);
-	unsigned long irq_flags;
+	struct rq_flags rf;
 	struct schedtune *st;
 	int idx;
 
@@ -527,7 +527,7 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu)
 	 * Boost group accouting is protected by a per-cpu lock and requires
 	 * interrupt to be disabled to avoid race conditions on...
 	 */
-	raw_spin_lock_irqsave(&bg->lock, irq_flags);
+	raw_spin_lock_irqsave(&bg->lock, rf.flags);
 	rcu_read_lock();
 
 	st = task_schedtune(p);
@@ -536,13 +536,13 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu)
 	schedtune_tasks_update(p, cpu, idx, DEQUEUE_TASK);
 
 	rcu_read_unlock();
-	raw_spin_unlock_irqrestore(&bg->lock, irq_flags);
+	raw_spin_unlock_irqrestore(&bg->lock, rf.flags);
 }
 
 void schedtune_exit_task(struct task_struct *tsk)
 {
 	struct schedtune *st;
-	unsigned long irq_flags;
+	struct rq_flags rf;
 	unsigned int cpu;
 	struct rq *rq;
 	int idx;
@@ -550,7 +550,7 @@ void schedtune_exit_task(struct task_struct *tsk)
 	if (!unlikely(schedtune_initialized))
 		return;
 
-	rq = lock_rq_of(tsk, &irq_flags);
+	rq = lock_rq_of(tsk, &rf);
 	rcu_read_lock();
 
 	cpu = cpu_of(rq);
@@ -559,7 +559,7 @@ void schedtune_exit_task(struct task_struct *tsk)
 	schedtune_tasks_update(tsk, cpu, idx, DEQUEUE_TASK);
 
 	rcu_read_unlock();
-	unlock_rq_of(rq, tsk, &irq_flags);
+	unlock_rq_of(rq, tsk, &rf);
 }
 
 int schedtune_cpu_boost(int cpu)
