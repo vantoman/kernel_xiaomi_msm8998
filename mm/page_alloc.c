8166a34c98a632e929994f74ebd4d355af13341f
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-04 18:04:32 -0700

    mm: Try harder for high-order allocations like in newer kernels
    In newer kernels, the page allocator retries a few times for high-order
    allocations instead of letting them fail so easily. Currently, there are
    more allocation failures than expected occurring for high-order
    allocations, so attempt to remedy them by trying harder.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Angga Satya Putra <anggasp@hotmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3152,7 +3152,7 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	unsigned long did_some_progress;
 	enum migrate_mode migration_mode = MIGRATE_ASYNC;
 	bool deferred_compaction = false;
-	int contended_compaction = COMPACT_CONTENDED_NONE;
+	int i, contended_compaction = COMPACT_CONTENDED_NONE;
 
 	/*
 	 * In the slowpath, we sanity check order to avoid ever trying to
@@ -3328,12 +3328,15 @@ noretry:
 	 * direct reclaim and reclaim/compaction depends on compaction
 	 * being called after reclaim so call directly if necessary
 	 */
-	page = __alloc_pages_direct_compact(gfp_mask, order, alloc_flags,
-					    ac, migration_mode,
-					    &contended_compaction,
-					    &deferred_compaction);
-	if (page)
-		goto got_pg;
+	for (i = order ? 4 : 0; i > 0; i--) {
+		page = __alloc_pages_direct_compact(gfp_mask, order,
+						    alloc_flags, ac,
+						    migration_mode,
+						    &contended_compaction,
+						    &deferred_compaction);
+		if (page)
+			goto got_pg;
+	}
 nopage:
 	warn_alloc_failed(gfp_mask, order, NULL);
 got_pg:
